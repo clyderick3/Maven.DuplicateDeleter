22352f673fe2cd74739ca755db94e3c87c78bf9b
Clyde Broderick <clyderick3@gmail.com>
2022-04-17 22:22:00 -0400

    Created constructor again. IntegerDuplicateDeleter.

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -7,8 +7,8 @@ import java.util.Arrays;
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
  */
 public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
-//    public IntegerDuplicateDeleter(Integer[] intArray) {
-//        super(intArray);
+    public IntegerDuplicateDeleter(Integer[] intArray) {
+        super(intArray);
     }
 
     @Override
